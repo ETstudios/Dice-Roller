85da8065f3b5af6dc37575947d6bc3f7b766b0f8
Eric Thomas <edt514@gmail.com>
2023-08-10 16:33:44 -0400

    Add DieOption.diceSides

--- Dice Roller/Assets/Scripts/Dice/DieOption.cs
+++ Dice Roller/Assets/Scripts/Dice/DieOption.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 /// <summary>
 /// Pointer script for dice mesh options.
 /// </summary>
-public class DieOption : MonoBehaviour { }
+public class DieOption : MonoBehaviour
+{
+    [SerializeField] private int diceSides;
+    public string Sides
+    {
+        get { return diceSides.ToString(); }
+        set { diceSides = int.Parse(value); }
+    }
+}
